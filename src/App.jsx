a8468e85097e60f2adaeb6daaeb16b4ff8d46085
khadizajarin <khadiza131310@gmail.com>
2023-12-10 21:38:34 +0600

    navbar aadded

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+import Navbar from "./components/Navbar"
+
+function App() {
+
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Navbar></Navbar>
+      
+      
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <a className="btn btn-ghost text-xl">Khadiza Jarin Roza</a>
+        </div>
+    );
+};
+
+export default Navbar;
